98587c73065dd3e4982012cee91cb47e81228a0a
el Tuba <tuba.linux@gmail.com>
2016-04-07 00:14:01 +0300

    Submit form.

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,12 +18,12 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://integrale.com/submit",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
-                    email: email,
+                    mail: email,
                     message: message
                 },
                 cache: false,
