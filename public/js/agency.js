7dcaa305d9e3135f9cc2f444d7b0b013d6142be1
Cheslav Novytskiy <novytskiua@Cheslavs-MacBook-Pro-2.local>
2016-04-07 19:31:02 +0300

    New graphic. Adaptivness improvements

--- public/js/agency.js
+++ public/js/agency.js
@@ -9,8 +9,8 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1300, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - 40
+        }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
